2e84324ffbf733ef9d88810b51dd8d02f0be2000
Laisla Schultz <laislaschultz@gmail.com>
2023-04-02 09:21:37 -0400

    assignment 7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import Button from "@mui/material/Button"
+import styled from 'styled-components'
 
 import './App.css';
 
@@ -6,17 +8,27 @@ import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
 
+const Title = styled.h1`
+color: orange;
+background: grey;
+text-decoration: underline;
+font-style: ${props => props.italic? "italic" : "normal"}
+`
+
+const StyledLink = styled(Link)`
+color: red;
+font-weight: 700;
+`
 
 function App() {
   
   const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
 
-
   return (
     <div className="App">
       <Router>
         <header>
-          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+          <Title italic={true}>Welcome to Monty's Mineral SPA</Title>
 
           <div className="navBar">
             <ul>
@@ -24,7 +36,7 @@ function App() {
                 <Link to="/">Home</Link>
               </li>
               <li>
-                <Link to="/about">About Us</Link>
+                <StyledLink to="/about">About Us</StyledLink>
               </li>
               <li>
                 <Link to="/packages">Our Packages</Link>
@@ -32,6 +44,8 @@ function App() {
             </ul>
           </div>
         </header>
+
+        <Button variant= "containerd" color="error"
 
         <div className="display">
           <Route path="/" component={Home} />
@@ -46,8 +60,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
